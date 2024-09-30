a3c3f784db9c6077ff3e7d22728205b51c1815d9
KenyNotKenny <keny7503@gmail.com>
2024-09-30 11:20:34 +0700

    Project stucture and dependency

--- core/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = hello;
-const world = 'MangaMeter';
-function hello(who = world) {
-    return `Hello ${who}! `;
-}
-console.log(hello());
