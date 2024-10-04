fbc8cdc79d32454a7dfad906a5e1028b1a12245a
KenyNotKenny <keny7503@gmail.com>
2024-10-04 21:05:20 +0700

    test db

--- core/src/tests/test.js
+++ core/src/tests/test.js
@@ -1,15 +1,20 @@
-const title = 'Kanojyo to Himitsu to Koimoyou';
+// const title = 'Kanojyo to Himitsu to Koimoyou';
 
-import axios from "axios";
+// import axios from "axios";
 
-const baseUrl = 'https://api.mangadex.org';
+// const baseUrl = 'https://api.mangadex.org';
 
-const resp = await axios({
-    method: 'GET',
-    url: `${baseUrl}/manga`,
-    params: {
-        title: title
-    }
-});
+// const resp = await axios({
+//     method: 'GET',
+//     url: `${baseUrl}/manga`,
+//     params: {
+//         title: title
+//     }
+// });
 
-console.log(resp.data.data.map(manga => manga.id));
+// console.log(resp.data.data.map(manga => manga.id));
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log(process.env.DB_USER);
