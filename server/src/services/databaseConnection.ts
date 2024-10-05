b3857892b44945d23857db3b231e8bc086a69c57
KenyNotKenny <keny7503@gmail.com>
2024-10-05 21:52:47 +0700

    genre

--- server/src/services/databaseConnection.ts
+++ server/src/services/databaseConnection.ts
@@ -1,22 +1,12 @@
 import { AppDataSource } from "./data-source"
-import { UserTest } from "../entity/Manga"
+import { Genre } from "../entity/Genre"
 require('dotenv').config()
+
 
 // console.log("test"+ process.env.DB_SERVER);
 export const runDatabaseConnection = AppDataSource.initialize().then(async () => {
 
-    console.log("Inserting a new user into the database...")
-    const user = new UserTest()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 30
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    console.log("Database connection established!");
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(UserTest)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
 
 }).catch(error => console.log(error))
