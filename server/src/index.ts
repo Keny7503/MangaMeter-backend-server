9b82a5c8b447f08da7700cdf171b2a3ea92ef4c2
KenyNotKenny <keny7503@gmail.com>
2024-10-05 15:06:46 +0700

    api working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,26 @@
-import { runDatabaseConnection } from "./services/databaseConnection";
-runDatabaseConnection;
+// import { runDatabaseConnection } from "./services/databaseConnection";
+// runDatabaseConnection;
+
+const express = require('express')
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+dotenv.config();
+const app = express()
+const port = 3000
+// Use Helmet for setting security-related HTTP headers
+app.use(helmet());
+
+// Use Morgan for logging HTTP requests
+app.use(morgan("dev"));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+import searchRoute from "./routes/search.js";
+app.use("/search", searchRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
