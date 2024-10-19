1ebde5326a1310114f1818cdfaf6241b5305d90b
KenyNotKenny <keny7503@gmail.com>
2024-10-19 15:36:15 +0700

    Update .gitignore to exclude dist and .env

--- dist/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (app) => {
-    // Define your routes here
-    app.get('/', (req, res) => {
-        res.send('Hello from the API');
-    });
-};
