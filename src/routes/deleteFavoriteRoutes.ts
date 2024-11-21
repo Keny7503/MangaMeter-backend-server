6229129b23d48575af7ab4388a7a15ac8b2c4b1c
“nameImuseless123” <stickboy7777@gmail.com>
2024-11-21 11:16:09 +0700

    fix delete favorite routes

--- src/routes/deleteFavoriteRoutes.ts
+++ src/routes/deleteFavoriteRoutes.ts
@@ -3,31 +3,31 @@ import { deleteFavoriteManga } from '../services/deleteFavoriteManga'; // Import
 
 const router = Router();
 
-// Define a route that uses query parameters for mangaName, mangaId, and genre
+// Define a route that uses query parameters for mangaId and userId
 router.delete("/", async (req, res) => {
     const mangaId = req.query.mangaId as string;
     const userId = req.query.userId as string;
 
     // Validate query parameters
     if (!mangaId || !userId) {
-        res.status(400).json({ error: "mangaId, userId is required query parameters" });
+        res.status(400).json({ error: "Both mangaId and userId are required query parameters." });
         return;
     }
 
     try {
         const response = await deleteFavoriteManga(mangaId, userId);
-        
-        // Send the response based on the result of addMangaWithGenres
-        if (!response) {
-            res.status(500).json({ error: "Failed to delete favorite manga" });
+        console.log("response: ",response)
+
+        if (response === null) { // Handle case where nothing was deleted
+            res.status(404).json({ message: "No matching favorite manga found to delete." });
             return;
         }
 
-        // Send the response based on the result of addMangaWithGenres
-        res.status(200).json(response);
+        // Successful deletion
+        res.status(200).json({ message: "Favorite manga deleted successfully.", data: response });
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: error});
+        console.error("Error deleting favorite manga:", error);
+        res.status(500).json({ error: "An internal server error occurred." });
     }
 });
 
